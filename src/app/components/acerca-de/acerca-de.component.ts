82e0ed703c8bd8871a8f0cab57bb413a356c1912
Jorge Heidenreich <88209188+jorgeheiden@users.noreply.github.com>
2022-05-01 23:15:35 -0300

    metodo Get funcionando

--- src/app/components/acerca-de/acerca-de.component.ts
+++ src/app/components/acerca-de/acerca-de.component.ts
@@ -10,14 +10,25 @@ export class AcercaDeComponent implements OnInit {
   /*1)Crear una variable: miPortfolio:any; */
   miPortfolio:any;
   educacionList:any;
+
   constructor(private datoPorfolio:PortfolioService) { }
 
+  nombres:any;
+
   ngOnInit(): void {
+
+    /* Este codigo se conecta al metodo get de una base de datos local, simulada en un JSON */
+    //this.datoPorfolio.obtenerDatos().subscribe(data =>{
+    //  console.log(data)
+    //  /*2)Asignar a la variable los valores obtenidos desde el json: this.miPortfolio=data;*/
+    //  this.miPortfolio=data;
+    //  this.educacionList=data.formacion_desarrollador;
+   // })
+
+        /*METODO GET*/
     this.datoPorfolio.obtenerDatos().subscribe(data =>{
       console.log(data)
-      /*2)Asignar a la variable los valores obtenidos desde el json: this.miPortfolio=data;*/
-      this.miPortfolio=data;
-      this.educacionList=data.formacion_desarrollador;
+      this.nombres = data;
     })
   }
 
--- src/app/servicios/portfolio.service.ts
+++ src/app/servicios/portfolio.service.ts
@@ -10,11 +10,18 @@ export class PortfolioService {
   /*3)Se crea un objeto llamado http, se Inyecta el modulo http Client (private http:HttpClient) */
   constructor(private http:HttpClient) { }
   
-  /* 1) Se crea una funcion obtener datos*/
-    /*5)Se define la funcion como Observable : Observable<any>*/
-  obtenerDatos(): Observable<any> {
-    /*6)se llama al metodo get.
-    Entre parentesis deberia ir un url de la API con el request */
-    return this.http.get('./assets/data/data.json');
-  }
+  /* Este codigo contiene un metodo get a una base de datos local, simulada en un JSON */
+  // 1) Se crea una funcion obtener datos*/
+  //5)Se define la funcion como Observable : Observable<any>
+  //obtenerDatos(): Observable<any> {
+  // 6)se llama al metodo get.
+  //  Entre parentesis deberia ir un url de la API con el request */
+  //  return this.http.get('./assets/data/data.json');
+  // }
+  
+
+  /*METODO GET*/
+    obtenerDatos(): Observable<any>{
+      return this.http.get<any>('https://api-con-springboot.herokuapp.com/ver/personas')
+    }
 }
