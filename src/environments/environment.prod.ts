a72eea7ea7134dff39a3db0b35c297d3ea81e685
Jorge Heidenreich <88209188+jorgeheiden@users.noreply.github.com>
2022-05-09 13:03:59 -0300

    1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBfP4R7q5EBratCntcnPu1W2j9-0vYm3eI",
+    authDomain: "imgproyectos-d5ba4.firebaseapp.com",
+    projectId: "imgproyectos-d5ba4",
+    storageBucket: "imgproyectos-d5ba4.appspot.com",
+    messagingSenderId: "266495819566",
+    appId: "1:266495819566:web:6f02587db1a3729fa0c4bf",
+    measurementId: "G-WCC0KP7734"
+  }
 };
