3e0af295186265b9fc5cb0578779effb58e6529c
Jorge Heidenreich <jmjhjorge@gmail.com>
2022-07-27 17:53:21 -0300

    primer commit entidad experiencia

--- src/app/components/experiencia-logged/experiencia-logged.component.ts
+++ src/app/components/experiencia-logged/experiencia-logged.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ExperienciaService } from 'src/app/servicios/experiencia.service';
+import { StorageService } from 'src/app/servicios/storage.service';
 
 @Component({
   selector: 'app-experiencia-logged',
@@ -7,9 +9,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienciaLoggedComponent implements OnInit {
 
-  constructor() { }
+  constructor(private storageService: StorageService, private experienciaService: ExperienciaService) { }
 
+  datos:any
   ngOnInit(): void {
+    this.experienciaService.obtenerDatos().subscribe( data =>{
+      this.datos = data
+      console.log("METODO GET TRABAJO: "+this.datos)
+    })
   }
 
+  //METODO POST
+  enviar(imagenTrab:any, nombreTrab:any, descripcionTrab:any){
+
+    this.experienciaService.enviarI({imagenTrab, nombreTrab, descripcionTrab}).subscribe(data =>{
+      console.log("METODO POST TRABAJO: " + data)
+      this.ngOnInit()
+    })
+  }
+
+  /*************METODO DELETE ***************/
+  eliminar(trabajo_id:any){
+    this.experienciaService.eliminarTrabajo(trabajo_id).subscribe((resultado:any) =>{
+
+      this.ngOnInit()
+    })
+  }
+
+    /*************METODO PUT *******************/
+    idHabilidad:any
+    obtenerIdTrabajo(id:any){
+      console.log("Este es el id: " + id)
+      this.idHabilidad = id
+    }
+    actualizar(idTrab:any, imagenTrab:any, nombreTrab:any, descripcionTrab:any){
+      this.experienciaService.actualizarTrabajo({idTrab, imagenTrab, nombreTrab, descripcionTrab}).subscribe(data =>{
+        console.log(data)
+        this.ngOnInit()
+      })
+    }
+
+  imagenes:any
+
+  /****************Codigo para subir imagenes a Firebase ****************/
+  // event: accede al evento
+  //event.target: accede al input
+  //event.target.files: accede a la carpeta files que es donde estan
+  //las imagenes que se suben
+
+  /******Funcion para obtener la imagen desde el HTML */
+
+  
+  cargarImagen(event:any){
+  
+    let archivos = event.target.files
+    //file reader/lector lee el archivo que ingresa
+    //y permite transformarlo en formato base 64
+    let nombre = "img"
+    
+    for(let i = 0; i < archivos.length; i++){
+      let reader = new FileReader();
+      reader.readAsDataURL(archivos[0]);
+      reader.onloadend = () =>{
+        this.storageService.subirImagen(nombre + Date.now(), reader.result).then(urlImagen=>{
+          
+          /**************LINK DE LA IMAGEN ***************/
+          console.log(urlImagen)
+          this.imagenes = urlImagen
+          
+        })
+        console.log(reader.result)
+      }
+
+
+    }
+ }
+
+ 
 }
--- src/app/servicios/experiencia.service.ts
+++ src/app/servicios/experiencia.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs'
 @Injectable({
   providedIn: 'root'
 })
 export class ExperienciaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  //METODO GET
+  obtenerDatos():Observable<any>{
+    return this.http.get<any>('https://back-end-arg-prog-2.herokuapp.com/trabajo/ver/trabajos')
+  }
+
+  //METODO POST
+  enviarI(dato:any): Observable<any>{
+    return this.http.post<any>('https://back-end-arg-prog-2.herokuapp.com/trabajo/nuevo',dato)
+}
+
+ /**************METODO DELETE ***************/
+ eliminarTrabajo(id:any):Observable<any>{
+  return this.http.delete(`${'https://back-end-arg-prog-2.herokuapp.com/trabajo/delete'}/${id}`)
+}
+
+ /*********METODO PUT ************/
+ actualizarTrabajo(dato:any):Observable<any>{
+  return this.http.put<any>('https://back-end-arg-prog-2.herokuapp.com/trabajo/update',dato)
+}
+
 }
